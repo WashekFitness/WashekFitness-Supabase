6ef8dc367d200f329bf90eee62faa5eddb549f8a
WashekFitness <washekfitness@gmail.com>
2026-08-28 22:38:25 -0400

    Refactor Toaster component to include dismiss function

--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -1,3 +1,4 @@
+
 import { useToast } from "@/components/ui/use-toast";
 import {
   Toast,
@@ -9,25 +10,52 @@ import {
 } from "@/components/ui/toast";
 
 export function Toaster() {
-  const { toasts } = useToast();
+  const {
+    toasts,
+    dismiss,
+  } = useToast();
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {toasts.map(function ({
+        id,
+        title,
+        description,
+        action,
+        ...props
+      }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast
+            key={id}
+            {...props}
+          >
             <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
+              {title && (
+                <ToastTitle>
+                  {title}
+                </ToastTitle>
+              )}
+
               {description && (
-                <ToastDescription>{description}</ToastDescription>
+                <ToastDescription>
+                  {description}
+                </ToastDescription>
               )}
             </div>
+
             {action}
-            <ToastClose />
+
+            <ToastClose
+              type="button"
+              onClick={() =>
+                dismiss(id)
+              }
+            />
           </Toast>
         );
       })}
+
       <ToastViewport />
     </ToastProvider>
   );
-} 
+}
