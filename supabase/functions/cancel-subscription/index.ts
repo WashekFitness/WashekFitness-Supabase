a31e45d70783473f28afcdc734702989ae88fbfc
WashekFitness <washekfitness@gmail.com>
2026-09-06 13:15:15 -0400

    Add fallback for SUPABASE_SERVICE_ROLE_KEY

--- supabase/functions/cancel-subscription/index.ts
+++ supabase/functions/cancel-subscription/index.ts
@@ -33,7 +33,9 @@ const SUPABASE_URL =
   Deno.env.get('SUPABASE_URL') || '';
 
 const SERVICE_ROLE_KEY =
-  Deno.env.get('SERVICE_ROLE_KEY') || '';
+  Deno.env.get('SERVICE_ROLE_KEY') ||
+  Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ||
+  '';
 
 const STRIPE_SECRET_KEY =
   Deno.env.get('STRIPE_SECRET_KEY') || '';
